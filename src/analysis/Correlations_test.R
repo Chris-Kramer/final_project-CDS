cf4f4b9a4de191d1833445893c6a40d73bcf4f60
Christoffer M. Kramer <33264251+Chris-Kramer@users.noreply.github.com>
2021-06-07 14:38:39 +0200

    changed correlations to make output

--- src/analysis/Correlations_test.R
+++ src/analysis/Correlations_test.R
@@ -7,7 +7,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Read data ---------------------------------------------------------------
 charged <- read_csv("../../data/processed_data/charges_per_10k.csv")
 reported <- read_csv("../../data/processed_data/charges_per_10k.csv")
-charged_reported <- read_csv("../data/processed_data/charged_vs_reported_pct.csv")
+charged_reported <- read_csv("../../data/processed_data/charged_vs_reported_pct.csv")
 pop_sq <- read_csv("../../data/processed_data/pop_sqkm.csv")
 tweets <- read_csv("../../data/processed_data/tweets_per_municipality.csv")
 
@@ -43,6 +43,12 @@ row.names(tweets) <- NULL
 
 # Function for performing correlations ------------------------------------
 calc_correlations <- function(start_col = 3, x_data, y_data, x_string, y_string) {
+  
+  df <- data.frame("p-val" = double(),
+                   "correlation_coefficient" = double(),
+                   "year" = integer(),
+                   "description" = character())
+  
   print(paste("----------- Correlations between", x_string, "and", y_string, "-------------------" ))
   for (i in 3:ncol(x_data)) {
     # Get column name
@@ -61,37 +67,46 @@ calc_correlations <- function(start_col = 3, x_data, y_data, x_string, y_string)
     result <- cor.test(x_vect, y_vect, method = c("pearson", "kendall", "spearman"))
     print(paste("Testing for corrlations between", x_string, "and", y_string,  "in Year", year, "..."))
     print(paste("P-Value:", result$p.value, "- Correlation Coefficient ", result$estimate))
+    
+    row <- data.frame("p-val" = as.double(result$p.value),
+                      "correlation_coefficient" = as.double(result$estimate),
+                      "year" = as.integer(year),
+                      "description" = "corrlations between", x_string, "and", y_string,  "in Year")
+    df <- rbind(df, row)
   }
-  
+  return(df)
 }
+
 
 # Test correlations -------------------------------------------------------
 # Correlation between charges and density
-calc_correlations(x_data = charged, y_data = pop_sq,
+df_output <- calc_correlations(x_data = charged, y_data = pop_sq,
                   x_string = "Charges",
                   y_string = "Population density")
 
 # Correlation between reports and density
-calc_correlations(x_data = reported, y_data = pop_sq,
-                  x_string = "Reports",
-                  y_string = "Population density")
+df_output <- rbind(df_output, calc_correlations(x_data = reported, y_data = pop_sq,
+                    x_string = "Reports",
+                    y_string = "Population density"))
 
 # Correlation between pct. of reports that leads to a charge and density
-calc_correlations(x_data = charged_reported, y_data = pop_sq,
-                  x_string = "Reports that results in a charge",
-                  y_string = "Population density")
+df_output <- rbind(df_output, calc_correlations(x_data = charged_reported, y_data = pop_sq,
+                    x_string = "Reports that results in a charge",
+                    y_string = "Population density"))
 
 # Correlations between tweets in a municipality and sexual assault charges
-calc_correlations(x_data = tweets, y_data = charged,
-                  x_string = "Charges",
-                  y_string = "Tweets in a municipality")
+df_output <- rbind(df_output, calc_correlations(x_data = tweets, y_data = charged,
+                    x_string = "Charges",
+                    y_string = "Tweets in a municipality"))
 
 # Correlations between tweets in a municipality and sexual assault reports
-calc_correlations(x_data = tweets, y_data = reported,
-                  x_string = "Reports",
-                  y_string = "Tweets in a municipality")
+df_output <- rbind(df_output, calc_correlations(x_data = tweets, y_data = reported,
+                    x_string = "Reports",
+                    y_string = "Tweets in a municipality"))
 
 # Correlation between pct. of reports that leads to a charge and tweets in a municipality
-calc_correlations(x_data = tweets, y_data = charged_reported,
-                  x_string = "Reports that results in a charge",
-                  y_string = "Tweets in a municipality")
+df_output <- rbind(df_output, calc_correlations(x_data = tweets, y_data = charged_reported,
+                    x_string = "Tweets in a municipality",
+                    y_string = "Reports that results in a charge"))
+
+write.csv(df_output, "../../output/correlations_test.csv")
